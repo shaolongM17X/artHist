685855c7bff5c527073f0203511564943881556b
shaolong <shaolonglin09@gmail.com>
2016-03-02 20:24:28 -0500

    add

--- artHist.js
+++ artHist.js
@@ -3,7 +3,19 @@ var ArtPieces = [
 		name: "Hours of Jeanne d'Evreaux",
 		Author: "Jean Pucelle",
 		period: "1324-1328",
-		src: "artPieces/bookofHours.jpg"
+		src: "artPieces/bookOfHours.jpg"
+	},
+	{
+		name: "virgin And Child",
+		Author: "Patron: Jeanne d’Evreaux",
+		period: "1324-1339",
+		src: "artPieces/virginAndChild.jpg"
+	},
+	{
+		name: "Saint Luke",
+		Author: "Master Theodoric",
+		period: "1360-1364",
+		src: "artPieces/saintLuke.jpg"
 	},
 	{
 		name: "Philip the Bold and Margaret of Flanders",
@@ -23,7 +35,222 @@ var ArtPieces = [
 		period: "Before 1482",
 		src: "artPieces/HoursofMaryofBurgundy.jpg"
 	},
-
+	{
+		name: "Rucellai Madonna",
+		Author: "Duccio",
+		period: "1285",
+		src: "artPieces/RucellaiMadonna.jpg"
+	},
+	{
+		name: "Ognissanti Madonna",
+		Author: "Giotto",
+		period: "1310",
+		src: "artPieces/OgnissantiMadonna.jpg"
+	},
+	{
+		name: "Lamentation",
+		Author: "Giotto",
+		period: "1305-06. Key TERM—Fresco",
+		src: "artPieces/Lamentation.jpg"
+	},
+	{
+		name: "Allegory of Good Government and The Effects of Good Government in the City and the Country,  Salla della Pace, Palazzo Pubblico",
+		Author: "Ambrogio Lorenzetti",
+		period: "1338-39",
+		src: "artPieces/goodgov.jpg"
+	},
+	{
+		name: "The Ghent Altarpiece,  Cathedral of Saint Bavo Ghent ",
+		Author: "Jan and Hubert? Van Eyck",
+		period: "1432 ",
+		src: "artPieces/ghaltar.jpg"
+	},
+	{
+		name: "The Ghent Altarpiece(inside),  Cathedral of Saint Bavo Ghent ",
+		Author: "Jan and Hubert? Van Eyck",
+		period: "1432 ",
+		src: "artPieces/grandaltar2.jpg"
+	},
+	{
+		name: "Man in a Red Turban",
+		Author: "Jan van Eyck",
+		period: "1433 ",
+		src: "artPieces/manredturban.jpg"
+	},
+	{
+		name: "The Arnolfini Wedding",
+		Author: "Jan van Eyck",
+		period: "1434",
+		src: "artPieces/arnolfini.jpg"
+	},
+	{
+		name: "Merode Altarpiece ",
+		Author: "Workshop  of  the Master of Flemalle",
+		period: "1425-30 ",
+		src: "artPieces/merodealtar.jpg"
+	},
+	{
+		name: "Deposition",
+		Author: "Rogier van der Weyden",
+		period: "1443",
+		src: "artPieces/deposition.jpg"
+	},
+	{
+		name: "Saint Luke Drawing the Virgin",
+		Author: "Rogier van der Weyden",
+		period: "1435-49.",
+		src: "artPieces/lukedrawingvirgin.jpg"
+	},
+	{
+		name: "Diptych of Marten van Nieuwernhove",
+		Author: "Hans Memling",
+		period: "1487",
+		src: "artPieces/diptychofnieuwemhove.jpg"
+	},
+	{
+		name: "The Reliquary of Saint Ursula",
+		Author: "Hans Memling",
+		period: "1489",
+		src: "artPieces/saintursula.jpg"
+	},
+	{
+		name: "The Buxheim Saint Christopher",
+		Author: "??",
+		period: "Mid-15th century. Key term: Woodcut",
+		src: "artPieces/TheBuxheimSaintChristopher.jpg"
+	},
+	{
+		name: "The Buxheim Saint Christopher",
+		Author: "Martin Schongauer",
+		period: "1470-1480. Engraving",
+		src: "artPieces/TheTemptationofSaintAnthony.jpg"
+	},
+	{
+		name: "Santa Maria del Fiore—Dome",
+		Author: "Filippo Brunelleschi",
+		period: "1420-36. Check slide6",
+		src: "artPieces/domeOfsantamariia.jpg"
+	},
+	{
+		name: "Ospidale Innocenti (Foundling Hospital)",
+		Author: "Filippo Brunelleschi",
+		period: "1419",
+		src: "artPieces/foundinghos.jpg"
+	},
+	{
+		name: "Saint George",
+		Author: "Donatello",
+		period: "1417-20",
+		src: "artPieces/SaintGeorge.jpg"
+	},
+	{
+		name: "Feast of Herod",
+		Author: "Donatello",
+		period: "1425",
+		src: "artPieces/FeastofHerod.jpg"
+	},
+	{
+		name: "Gates of Paradise",
+		Author: "Lorenzo Ghiberti",
+		period: "1425-52",
+		src: "artPieces/GatesofParadise.jpg"
+	},
+	{
+		name: "David",
+		Author: "Donatello",
+		period: "1446-60",
+		src: "artPieces/david.jpg"
+	},
+	{
+		name: "The Expulsion of Adam and Eve from Paradise",
+		Author: "Masaccio",
+		period: "1427",
+		src: "artPieces/expulsionofAdamandEveMasaccio.jpg"
+	},
+	{
+		name: "The Tribute Money, Florence",
+		Author: "Masaccio",
+		period: "1427",
+		src: "artPieces/TheTributeMoney.jpg",
+	},
+	{
+		name: "Trinity with the Virgin, Saint John the Evangelist, and Donors, Florance",
+		Author: "Masaccio",
+		period: "1425-27",
+		src: "artPieces/trinity.jpg",
+	},
+	{
+		name: "Annunciation, Florence",
+		Author: "Fra Angelico",
+		period: "1438-45",
+		src: "artPieces/annunciationofAngelico.jpg",
+	},
+	{
+		name: "Battista Sforza and Federico da Montefeltro",
+		Author: "Piero della Francesca",
+		period: "1474",
+		src: "artPieces/battistasforzaandfederico.jpg",
+	},
+	{
+		name: "Primavera",
+		Author: "Sandro Botticelli",
+		period: "1482",
+		src: "artPieces/Primavera.jpg",
+	},
+	{
+		name: "Saint Francis in Ecstasy",
+		Author: "Giovanni Bellini",
+		period: "1470s",
+		src: "artPieces/saintfrancisinecstasy.jpg",
+	},
+	{
+		name: "Saint Francis in Ecstasy",
+		Author: "Giovanni Bellini",
+		period: "1470s",
+		src: "artPieces/saintfrancisinecstasy.jpg",
+	},
+	{
+		name: "The Madonna of the Rocks",
+		Author: "Leonardo da Vinci",
+		period: "1485",
+		src: "artPieces/MADONNAoftherock.jpg",
+	},
+	{
+		name: "The last supper",
+		Author: "Leonardo da Vinci",
+		period: "1495-98",
+		src: "artPieces/lastsupper.jpg",
+	},
+	{
+		name: "Mona Lisa",
+		Author: "Leonardo da Vinci",
+		period: "1503-06",
+		src: "artPieces/monalisa.jpg",
+	},
+	{
+		name: "Madonna of the Goldfinch",
+		Author: "Raphael",
+		period: "1506",
+		src: "artPieces/Raphaelmadonnaofthegoldfinch.jpg",
+	},
+	{
+		name: "The School of Athens",
+		Author: "Raphael",
+		period: "1510-11",
+		src: "artPieces/TheSchoolofAthens.jpg",
+	},
+	{
+		name: "Baldassare",
+		Author: "Raphael",
+		period: "1515. Compare it with Mona Lisa",
+		src: "artPieces/Baldassare.jpg",
+	},
+	{
+		name: "Cartoon for the tapestry portraying Christ’s Charge to Peter",
+		Author: "Raphael",
+		period: "1515-16",
+		src: "artPieces/CartoonforthetapestryportrayingChrist’sChargetoPeter.jpg",
+	},
 ];
 
 var answer_button = $("#show-answer");
@@ -56,6 +283,7 @@ $(document).keypress(function(event){
 		$(".answer_part").hide();
 		$("#artist-input").val('');
 		$("#period-input").val(''); 
+		console.log("cur pos is "+titles.indexOf(currentArtPiece));
 		var next_pos = titles.indexOf(currentArtPiece) + 1;
 		if (next_pos == titles.length) {
 			next_pos = 0;
@@ -66,8 +294,24 @@ $(document).keypress(function(event){
 		$("#art_title").html(next_work.name);
 		$("#img").attr("src", next_work.src);
 		currentArtPiece = next_work.name;
+		console.log(next_pos);
 	}
-	if (event.keyCode == 91) {
+	if (event.keyCode == 91) {		
+		$(".answer_part").hide();
+		$("#artist-input").val('');
+		$("#period-input").val(''); 
+		var next_pos = titles.indexOf(currentArtPiece) - 1;
+		if (next_pos == -1) {
+			next_pos = titles.length - 1;
+		}
+		var next_work = ArtPieces[next_pos];
+		$("#artist-name").html(next_work.Author);
+		$("#art-period").html(next_work.period);
+		$("#art_title").html(next_work.name);
+		$("#img").attr("src", next_work.src);
+		currentArtPiece = next_work.name;
+	}
+	if (event.keyCode == 92) {
 		$(".answer_part").show();
 	}
 	
@@ -82,6 +326,5 @@ for (var i = 0; i < ArtPieces.length; i++) {
 	$("#"+i).click(function(event){
 		var currentID = this.id;
 		currentArtPiece = ArtPieces[currentID-1].name;
-		console.log(currentArtPiece);
 	});
 }
